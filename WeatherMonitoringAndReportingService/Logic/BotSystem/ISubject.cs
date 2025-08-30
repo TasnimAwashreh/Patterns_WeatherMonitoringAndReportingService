c2ac72575b6456b4edf07b5a2b07a71539b5e3ab
Tasnim Awashreh <Tasnim.awashreh@foothillsolutions.com>
2025-08-30 21:52:08 +0300

    - Implemented Observer Pattern for the subject and bots

---
+++ WeatherMonitoringAndReportingService/Logic/BotSystem/ISubject.cs
@@ -0,0 +1,12 @@
+﻿using WeatherMonitoringAndReportingService.Data.Models;
+
+namespace WeatherMonitoringAndReportingService.Logic.BotSystem
+{
+    public interface ISubject
+    {
+        public void Attach(IBotObserver observer);
+        public void Detach(IBotObserver observer);
+        public void Notify();
+        public void ProcessNewData(WeatherData weatherData);
+    }
+}
