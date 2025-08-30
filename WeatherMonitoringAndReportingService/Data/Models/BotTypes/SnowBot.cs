44cdd6edb388eb5b8a867a691b448ba413a0db69
Tasnim Awashreh <Tasnim.awashreh@foothillsolutions.com>
2025-08-30 21:52:08 +0300

    - Implemented Observer Pattern for the subject and bots

---
+++ WeatherMonitoringAndReportingService/Data/Models/BotTypes/SnowBot.cs
@@ -0,0 +1,18 @@
+﻿using WeatherMonitoringAndReportingService.Logic.BotSystem;
+
+namespace WeatherMonitoringAndReportingService.Data.Models.Bots
+{
+    public class SnowBot : Bot, IBotObserver
+    {
+        public SnowBot(string message, float threshold, bool isEnabled) : base(message, threshold, isEnabled)
+        {
+        }
+
+        public void Update(ISubject subject)
+        {
+            var temperature = (subject as Subject).weatherData.Temperature;
+            if (temperature > Threshold)
+                Console.WriteLine(Message);
+        }
+    }
+}
---
+++ WeatherMonitoringAndReportingService/Data/Models/BotTypes/SunBot.cs
@@ -0,0 +1,18 @@
+﻿using WeatherMonitoringAndReportingService.Logic.BotSystem;
+
+namespace WeatherMonitoringAndReportingService.Data.Models.Bots
+{
+    public class SunBot : Bot, IBotObserver
+    {
+        public SunBot(string message, float threshold, bool isEnabled) : base(message, threshold, isEnabled)
+        {
+        }
+
+        public void Update(ISubject subject)
+        {
+            var temperature = (subject as Subject).weatherData.Temperature;
+            if (temperature > Threshold)
+                Console.WriteLine(Message);
+        }
+    }
+}
