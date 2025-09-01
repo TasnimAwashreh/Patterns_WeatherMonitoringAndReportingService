4a6e1c422a918abd48c1e25c9e16869deebc34d6
Tasnim Awashreh <Tasnim.awashreh@foothillsolutions.com>
2025-09-01 09:57:59 +0300

    - New branch for PR

--- WeatherMonitoringAndReportingService/Logic/Readers/ChooseFormatReader.cs
+++ WeatherMonitoringAndReportingService/Logic/Readers/ChooseFormatReader.cs
@@ -1,12 +1,11 @@
-﻿using System;
-namespace WeatherMonitoringAndReportingService.Logic.Readers
+﻿namespace WeatherMonitoringAndReportingService.Logic.Readers
 {
     public static class ChooseFormatReader
     {
         public static IFormatReader ChooseReader(string input)
         {
             input = input.Trim();
-            if(input[0].CompareTo('<') == 0)
+            if (input[0].CompareTo('<') == 0)
                 return new XMLFormatReader();
             else
                 return new JsonFormatReader();
