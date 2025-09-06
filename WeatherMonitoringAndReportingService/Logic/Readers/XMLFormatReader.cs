1dfecb7d856db9195e4d92bcf691f8cd034038c4
Tasnim Awashreh <Tasnim.awashreh@foothillsolutions.com>
2025-09-06 22:41:36 +0300

    - Surrounded readers with try catch

--- WeatherMonitoringAndReportingService/Logic/Readers/XMLFormatReader.cs
+++ WeatherMonitoringAndReportingService/Logic/Readers/XMLFormatReader.cs
@@ -7,10 +7,17 @@ namespace WeatherMonitoringAndReportingService.Logic.Readers
     {
         public WeatherData? ReadWeatherData(string userInput)
         {
-            var serializer = new XmlSerializer(typeof(WeatherData));
-            using var reader = new StringReader(userInput);
-            WeatherData data = (WeatherData)serializer.Deserialize(reader);
-            return data;
+            try
+            {
+                var serializer = new XmlSerializer(typeof(WeatherData));
+                using var reader = new StringReader(userInput);
+                WeatherData data = (WeatherData)serializer.Deserialize(reader);
+                return data;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
         }
     }
 }
