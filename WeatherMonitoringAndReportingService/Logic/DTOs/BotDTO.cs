60b23ba6af18e1db98ad6874c82c15eef80b139c
Tasnim Awashreh <Tasnim.awashreh@foothillsolutions.com>
2025-09-06 22:33:24 +0300

    - Added DTOs

---
+++ WeatherMonitoringAndReportingService/Logic/DTOs/BotDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherMonitoringAndReportingService.Logic.DTOs
+{
+    public class BotDTO
+    {
+        public string Message { get; set; }
+        public bool Enabled { get; set; }
+    }
+}
---
+++ WeatherMonitoringAndReportingService/Logic/DTOs/SunBotDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherMonitoringAndReportingService.Logic.DTOs
+{
+    public class SunBotDTO : BotDTO
+    {
+        public float TemperatureThreshold;
+    }
+}
